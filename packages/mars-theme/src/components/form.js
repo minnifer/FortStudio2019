6f2679db2cf9e4d6a783fa7fbcb568f7bf5f1aa9
Fort Yeah - Min <min@fortyeah.com>
2019-12-19 09:28:34 -0500

    fixed form cursor

--- packages/mars-theme/src/components/form.js
+++ packages/mars-theme/src/components/form.js
@@ -87,6 +87,10 @@ const PageContainer = styled.div`
   min-height: 100vh;
   cursor: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E %3Ccircle id='Cursor' cx='6' cy='6' r='6' fill='%231D1D1D' opacity='0.9'/%3E %3C/svg%3E "),
     pointer !important; 
+    *{
+       cursor: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E %3Ccircle id='Cursor' cx='6' cy='6' r='6' fill='%231D1D1D' opacity='0.9'/%3E %3C/svg%3E "),
+    pointer !important; 
+    }
 `;
 const RightContainer = styled.div`
   width: 40%;
